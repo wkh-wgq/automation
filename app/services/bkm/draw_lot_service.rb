c5acf62e75d4a0a396a10f71d3aa8164b1575b22
wangkaihua <16621142402@163.com>
2025-04-08 09:24:04 +0800

    调整抽奖逻辑

--- app/services/bkm/draw_lot_service.rb
+++ app/services/bkm/draw_lot_service.rb
@@ -6,7 +6,7 @@ module Bkm
       super
       # user_agent = Faker::Internet.user_agent
       @playwright_exec = Playwright.create(playwright_cli_executable_path: "./node_modules/.bin/playwright")
-      @browser = @playwright_exec.playwright.chromium.launch(headless: true)
+      @browser = @playwright_exec.playwright.chromium.launch(headless: false)
       @page = @browser.new_page
       @login_retry_count = 0
     end
@@ -46,13 +46,17 @@ module Bkm
     def queue_up
       return if page.title != QUEUE_UP_TITLE
       logger.info "用户(#{virtual_user.email})开始排队"
-      while !page.locator("#buttonConfirmRedirect").visible?
+      while (page.wait_for_load_state(state: "load"); page.title == QUEUE_UP_TITLE)
         sleep 5
       end
+      # while !page.locator("#buttonConfirmRedirect").visible?
+      #   sleep 5
+      # end
       logger.info "用户(#{virtual_user.email})排队完成"
+      sleep rand(4.0..8.0)
       # 点击进入网站按钮
-      page.locator("#buttonConfirmRedirect").click
-      logger.info "用户(#{virtual_user.email})进入网站"
+      # page.locator("#buttonConfirmRedirect").click
+      # logger.info "用户(#{virtual_user.email})进入网站"
     end
 
     def draw_lot
@@ -62,7 +66,7 @@ module Bkm
       sleep(rand(0.5..2.0))
       page.click("p.radio label")
       sleep(rand(0.5..2.0))
-      page.locator("#L0000000003").click
+      page.locator("#L0000000004").click
       sleep(rand(0.5..2.0))
       page.click("a.popup-modal.on")
       sleep(rand(0.5..2.0))
@@ -73,6 +77,16 @@ module Bkm
       sleep(rand(2.0..4.0))
       page.goto "https://www.pokemoncenter-online.com/lottery-history/"
       sleep(rand(3.0..5.0))
+    end
+
+    def execute_with_log(method)
+      logger.debug "用户(#{virtual_user.email})抢单-(#{method})流程开始"
+      send(method)
+      logger.debug "用户(#{virtual_user.email})抢单-(#{method})流程结束"
+    rescue Exception => e
+      logger.error "用户(#{virtual_user.email})抢单-(#{method})流程异常：#{e.message}"
+      record.update(failed_step: method, error_message: e.message)
+      raise e
     end
   end
 end
